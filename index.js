111f6b49a7ccc0bbf72bf86b7cdf37dfb481ae9e
Kelvin M <kelkm609@gmail.com>
2024-03-20 08:21:13 -0400

    Updated with code

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 require('dotenv').config();
-
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
+
+async function getImage(query){
+    const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=tpcZT20FiNlXIQI00DxNBo1mCVGPob15&q=dog&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+
+   try {
+    const response = await fetch(endpoint)
+    const data = await response.json()
+    const imageUrl = data.data[0]
+    return imageUrl
+    }
+    catch (error){
+    console.error("Error fetching image")
+    throw error;
+    }
+
+}
+console.log(getImage())
